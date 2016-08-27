5924dff38e4d94aa2853d3ab0bd5ae4e981dffbd
lizhunzhun <445692962@qq.com>
2016-08-27 11:37:05 +0800

    0.0.8

--- src/com/baidu/Test.java
+++ src/com/baidu/Test.java
@@ -2,7 +2,6 @@ package com.baidu;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("aibiancheng");
-		System.out.println("Helloaaaaa");
+		System.out.println("Helloaaaaabbbbb");
 	}
 }
